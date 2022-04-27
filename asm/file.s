3e0fa35f5e90c0085d3cfda2774468890010848b
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2022-04-27 13:11:32 -0400

    repair lcf

--- asm/file.s
+++ asm/file.s
@@ -1,6 +1,6 @@
 .include "macros.inc"
 
-.section .file, "wa"  # 0x800E6380 - 0x800E63A0
+.section ._file, "wa"  # 0x800E6380 - 0x800E63A0
 	# ROM: 0xE3380
 	.asciz "Ver.05/07/23-11:48:04"
 	.balign 4
