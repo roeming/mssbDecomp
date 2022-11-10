562f2ef3c78c7600f583bd0a2efec94b52853525
roeming <brother64youyou@gmail.com>
2022-11-10 14:00:01 -0500

    reverting changes for ai.c

---
+++ include/sdk/ai.h
@@ -0,0 +1,30 @@
+// #ifndef __AI_H_
+// #define __AI_H_
+
+// #include "types.h"
+
+// void AIRegisterDMACallback(void);
+// // void AIInitDMA(void);
+// // void AIStartDMA(void);
+// // void AIStopDMA(void);
+// // void AIGetDMAStartAddr(void);
+// // void AIRegisterStreamCallback(void);
+// // void AIGetStreamSampleCount(void);
+// // void AIResetStreamSampleCount(void);
+// // void AISetStreamPlayState(void);
+// // void AIGetStreamPlayState(void);
+// // void AISetDSPSampleRate(void);
+// // void AIGetDSPSampleRate(void);
+// // void __AI_set_stream_sample_rate(void);
+// // void AIGetStreamSampleRate(void);
+// // void AISetStreamVolLeft(void);
+// // void AIGetStreamVolLeft(void);
+// // void AISetStreamVolRight(void);
+// // void AIGetStreamVolRight(void);
+// // void AIInit(void);
+// // void __AISHandler(void);
+// // void __AIDHandler(void);
+// // void __AICallbackStackSwitch(void);
+// // void __AI_SRC_INIT(void);
+
+// #endif
